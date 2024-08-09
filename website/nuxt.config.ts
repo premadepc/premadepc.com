2aa112d099713c5ee7c5ecec5fffe96bbe3d3f34
Philip Kelly <wizphil@gmail.com>
2024-08-08 18:28:40 -0700

    Updating dockerfile and configs

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,7 +1,7 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2024-08-03', // Updated to today's date
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   // Global page headers
   app: {
     head: {
